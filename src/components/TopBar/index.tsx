32f4fbf7476422d2c5b49c22237677b62917da12
kgrasewicz <grasewiczkatarzyna@gmail.com>
2024-12-08 12:38:31 +0100

    Fix styling

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,5 +1,5 @@
 const TopBar = () => {
-  return <div className="h-12 w-full bg-black left-0 top-0 fixed" />;
+  return <div className="h-12 w-full bg-black left-0 top-0 sticky" />;
 };
 
 export default TopBar;
