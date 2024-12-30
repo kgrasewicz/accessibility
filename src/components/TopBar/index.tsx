20a7eb1be3c12467716c907a85dcf76c29d8f328
kgrasewicz <grasewiczkatarzyna@gmail.com>
2024-12-30 17:08:30 +0100

    Add logo

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,5 +1,15 @@
+import Logo from "src/assets/logo.svg?react";
+
 const TopBar = () => {
-  return <div className="h-12 w-full bg-grey-900 left-0 top-0 fixed z-10" />;
+  return (
+    <div className="h-12 w-full bg-grey-900 left-0 top-0 fixed p-2 z-10">
+      <div className="flex lg:max-w-[700px] xl:max-w-[1056px] mx-auto">
+        <a href="https://www.filmweb.pl/" aria-label="Filmweb">
+          <Logo className="h-8" aria-hidden />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
