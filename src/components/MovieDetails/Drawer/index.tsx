6d039c97f907b2a98f5be52f9d9edb498a4987d9
kgrasewicz <grasewiczkatarzyna@gmail.com>
2025-01-11 12:03:12 +0100

    Fix drawer width for mobile

--- src/components/MovieDetails/Drawer/index.tsx
+++ src/components/MovieDetails/Drawer/index.tsx
@@ -17,7 +17,7 @@ const Drawer = ({ open, onClose, title, children }: DrawerProps) => {
       direction="right"
       onClose={onClose}
     >
-      <div className="w-[580px] px-5">
+      <div className="w-screen max-w-[580px] px-5">
         <div className="py-4 flex justify-between border-b-[1px] border-grey-200 items-center">
           <div className="lato-regular text-xl">{title}</div>
           <button onClick={onClose} aria-label="Close">
